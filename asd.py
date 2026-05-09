af498d677f9781d0aa00e3ce93b395fbe5129008
Michell <michelltorres1612@gmail.com>
2026-05-09 05:54:02 -0400

    Pequeñas modificaciones para preparar los estados de cada cuenta

--- asd.py
+++ asd.py
@@ -11,8 +11,10 @@ class Verificador(Password,Nombre):
 
             if self.nombre != x:
                 return False
+                break
             elif self.nombre == x:
                 return True
+                break
 
         return None
 
@@ -22,8 +24,10 @@ class Verificador(Password,Nombre):
 
             if self.passw != x:
                 return False
+                break
             elif self.passw == x:
                 return True
+                break
 
         return None
 def verificar_gmail(gmail):
@@ -32,21 +36,30 @@ def verificar_gmail(gmail):
     else:
         return True
 
+def establecer_estado(nuevo_estado):
+    estado = nuevo_estado
+    return estado
+
+
+
+
+
 class Login(Nombre,Password):
     def __init__(self,_nombre,_password):
         Nombre.__init__(self, _nombre)
         Password.__init__(self, _password)
-
+        return self.nombre,self.passw
 
 
 
 class Register(Nombre,Password,Gmail,Edad):
-    def __init__(self,_nombre,_edad,_gmail,_password,_id):
+    def __init__(self,_nombre,_edad,_gmail,_password,_id,_estado):
 
         Nombre.__init__(self, _nombre)
         Edad.__init__(self,_edad)
         Gmail.__init__(self,_gmail)
         Password.__init__(self, _password)
+        self.estado = _estado
         self.id = _id
 
     def __repr__(self):
